1effaaa41b2756711bf57b40bf1568788fedef5a
ralf <kingarthas37@hotmail.com>
2016-04-14 11:25:30 +0800

    4.14-1

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 
 app.use(AV.Cloud.CookieSession({secret: 'secret-av', maxAge: 1000*60*60*24, fetchUser: true}));
 
-app.use(session({secret: 'secret'}));
+app.use(session({secret:'secret',resave:true,saveUninitialized: true}));
 app.use(require('connect-flash')());
 
 // 未处理异常捕获 middleware
