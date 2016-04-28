67347daba16d83f3a8574a6663c1fea9b7d54e4d
ralf <kingarthas37@hotmail.com>
2016-04-28 17:23:43 +0800

    4.28

--- routes/main.js
+++ routes/main.js
@@ -5,18 +5,18 @@ require('express').Router();
 module.exports = {
     
     //admin
-    '/admin/login':require('./admin/sign/login'),
-    '/admin/register':require('./admin/sign/register'),
-    '/admin/logout':require('./admin/sign/logout'),
+    //'/admin/login':require('./admin/sign/login'),
+    //'/admin/register':require('./admin/sign/register'),
+    //'/admin/logout':require('./admin/sign/logout'),
     
-    '/admin':require('./admin/index'),
-
-    '/admin/product':require('./admin/product'),
-    '/admin/product/add':require('./admin/product/add'),
-    '/admin/product/edit':require('./admin/product/edit'),
-    '/admin/product/upload':require('./admin/product/upload'),
-    //'/admin/product/preview':require('./admin/product/preview')
-   
-    '/admin/product-category':require('./admin/product-category')
+    '/admin':require('./admin/index')
+    //
+    //'/admin/product':require('./admin/product'),
+    //'/admin/product/add':require('./admin/product/add'),
+    //'/admin/product/edit':require('./admin/product/edit'),
+    //'/admin/product/upload':require('./admin/product/upload'),
+    ////'/admin/product/preview':require('./admin/product/preview')
+    //
+    //'/admin/product-category':require('./admin/product-category')
     
 };
