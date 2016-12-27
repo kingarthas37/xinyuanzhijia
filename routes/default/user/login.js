43290c1bd000563aa027f995887e32ef907ff9c4
Charles Wu <chwu@ebates.com>
2016-12-27 15:07:25 +0800

    wechat login

--- routes/default/user/login.js
+++ routes/default/user/login.js
@@ -1,18 +1,14 @@
 'use strict';
 
-let router = require('express').Router();
-
-let async = require('async');
-let extend = require('xtend');
-
-let config = require('../../../lib/config');
-
-let user = require('../../../lib/models/user');
-let AV = require('leanengine');
-user.createNew();
+let user = require('../../../lib/models/user').createNew();
+let request = user.getRequest();
+let config = user.getConfig();
+let router = user.getRouter();
 
 router.get('/', (req, res) => {
-    res.render('default/user/login');
+    let wechatLoginUrl = config.wechatApi.authorize;
+    wechatLoginUrl = wechatLoginUrl.replace('{appid}', config.wechatConfig.appId).replace('{redirectUrl}', config.website.domain).replace('{scopt}', 'snsapi_userinfo').replace('{state}', '51wish');
+    res.render('default/user/login', {wechatLoginUrl:wechatLoginUrl});
 
 });
 
@@ -35,6 +31,27 @@ router.get('/getSmsCode/:mobile', (req, res) => {
     user.requestSmsCode(mobile).then(data=> {
         res.send(data);
     });
+});
+
+router.get('/wechatLogin', (req, res) => {
+    let code = req.query.code;
+    if (typeof(code) == 'undefined') {
+        res.send('error');
+    } else {
+        let url = config.wechatApi.authorizeAccessToken;
+        url = url.replace('{appid}', config.wechatConfig.appId).replace('{secret}', config.wechatConfig.appSecret).replace('{code}', code);
+        request(url, function (error, response, body) {
+            if (response.code != 200 || error) {
+                res.redirect('/');
+            }
+            if (typeof(body.openid) == 'undefined' || typeof(body.access_token) == 'undefined') {
+                res.redirect('/');
+            }
+            user.singInWithWechat(body.openid, body.access_token).then(data=>{
+                res.send(data);
+            });
+        })
+    }
 });
 
 
