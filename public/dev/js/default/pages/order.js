ccd29eb77fe60a6263c9f043ee509bf388b28a0c
Ralf Wang <rwang@ebates.com>
2017-03-01 18:29:21 +0800

    3.1

--- public/dev/js/default/pages/order.js
+++ public/dev/js/default/pages/order.js
@@ -4,7 +4,55 @@ module.exports = {
     
     expressFun() {
 
-        console.info(222);
+        let modalSearch = $('#modal-search');
+        let modalResult = $('#modal-result');
+        
+        $('.search-tracking').click(function() {
+            let tracking = $(this).data('tracking');
+            let express = $(this).data('express');
+            
+            modalSearch.modal();
+            
+            $.ajax({
+                type:'get',
+                url:`/order/express/query/${tracking}/${express}`
+            }).then(data => {
+
+                let template = '';
+                if(data.list) {
+                    for(let i=0;i<data.list.length;i++) {
+                        let last = i === data.list.length-1 ? 'color-timeline-item-last' : '';
+                        let color = i === 0 ? 'green' : 'blue';
+                        template += `
+                            <li class="color-timeline-item ${last}">
+                                <div class="color-timeline-item-tail"></div>
+                                <div class="color-timeline-item-head color-timeline-item-head-${color}"></div>
+                                <div class="color-timeline-item-content"><strong>${data.list[i].time}</strong> ${data.list[i].status}</div>
+                            </li>
+                        `;
+                    }
+                } else {
+                    template = `
+                        <li class="color-timeline-item">
+                            <div class="color-timeline-item-tail"></div>
+                            <div class="color-timeline-item-head color-timeline-item-head-red"></div>
+                            <div class="color-timeline-item-content">${data.msg}</div>
+                        </li>
+                    `;
+                }
+                modalSearch.modal('close');
+
+                modalResult.find('ul').html(template);
+                modalResult.modal();
+            }, error => {
+                modalSearch.modal('close');
+                modalResult.find('ul').html(error);
+                modalResult.modal();
+            });
+
+            
+            
+        });
         
     }
 
