aa59b6b1c3da5a07a406285c5ac42450022ed630
Ralf Wang <rwang@ebates.com>
2017-10-26 18:07:59 +0800

    git

--- public/dev/js/default/common/common-dom.js
+++ public/dev/js/default/common/common-dom.js
@@ -208,7 +208,7 @@ $(function() {
             let content = '';
             $.each(data.items,function(i,item) {
                 
-                let price = item.price > 0 ? parseFloat(item.price) : 0.00;
+                let price = item.price > 0 ? item.price +'.00' : '-';
                 let image = (()=> {
                     let img = '';
                     for(let n in item.mainImage) {
@@ -224,6 +224,9 @@ $(function() {
                 let isRefund = item.isRefund ? '<span class="active"><i class="am-icon-history"></i>7天退款</span>' : '';
                 let isHandmade = item.isHandmade ? '<span class="active"><i class="am-icon-modx"></i>纯手工</span>' : '';
                 let isOnly = item.isOnly ? '<span class="active"><i class="am-icon-gavel"></i>只此一件 </span>' : '';
+                let isStockText = item.stock > 0 ? '[现货]' : '';
+                let isShortStockText = item.isShortStock ? '[缺货]' : '';
+                let isShortStockCss = item.isShortStock ? 'class="out-stock"' : '';
                 
                 content += `
                     <li>
@@ -231,13 +234,17 @@ $(function() {
                             <a href="/product/detail/${item.productId}"><img src="//ac-JoaBcRTt.clouddn.com/3a994354f637e827ae7e.png" class="lazy" width="100" height="100" data-original="${image}?imageMogr2/thumbnail/200"></a>
                         </div>
                         <div class="detail">
-                            <h3><a href="/product/detail/${item.productId}">${item.name}</a></h3>
+                            <h3>
+                                <a href="/product/detail/${item.productId}" ${isShortStockCss}>
+                                  ${isStockText}${isShortStockText} ${item.name}
+                                </a>
+                            </h3>
                             <p>
-                            <!--<span class="price">¥ <strong>${price}</strong></span>-->
+                            <span class="price">¥ <strong>${price}</strong></span>
                             <span>${item.pageViews}次浏览</span>
                             </p>
                             <p>
-                                ${stock} ${isRefund} ${isHandmade} ${isOnly}
+                                ${isRefund} ${stock} ${isHandmade} ${isOnly}
                             </p>
                         </div>
                     </li>
