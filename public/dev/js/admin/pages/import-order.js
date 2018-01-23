7130757c38a56946b3758c0be7c131206f1dff5a
Ralf Wang <ralf@extrabux.com>
2018-01-23 22:15:55 +0800

    git

--- public/dev/js/admin/pages/import-order.js
+++ public/dev/js/admin/pages/import-order.js
@@ -28,16 +28,31 @@ module.exports = {
                 });
             });
 
+            let countLen = 1;
+            let $countLen = $('.count-len');
+            $countLen.text(`${countLen}/${arrWisdomData.length}`);
             async.forEachSeries(arrWisdomData, function(item, callback) {
                 $.ajax({
                     url:'/admin/import-order/data',
                     type:'post',
                     data:{'import-data': JSON.stringify([item])}
                 }).then(function (data) {
+                    $countLen.text(`${countLen}/${arrWisdomData.length}`);
+                    countLen ++;
+                    let mainImage = 'http://ac-QuiPuWpJ.clouddn.com/0d56f9d95dc6da7edf45.png';
+                    for(let i in data.result[0].mainImage) {
+                        if(data.result[0].mainImage[i].isMainImage) {
+                            mainImage = data.result[0].mainImage[i].url;
+                        }
+                    }
+
                     modalOrderSearchContent.append(`<tr>
-                        <td>${data.result[0].name}</td>
-                        <td>${data.result[0].count}</td>
-                        <td><span class="import-check-true"><i class="am-icon-check"></i> 可导入</span></td>
+                        <td class="t-c"><a target="_blank" href="${mainImage}"><img src="${mainImage}?imageMogr2/thumbnail/32" alt=""></a></td>
+                        <td>${data.result[0].nameCn}</td>
+                        <td class="t-c">${data.result[0].stock}</td>
+                        <td class="t-c">${data.result[0].reserve}</td>
+                        <td class="t-c"><strong>${data.result[0].count}</strong></td>
+                        <td class="t-c"><span class="import-check-true"><i class="am-icon-check"></i> 可导入</span></td>
                     </tr>`);
                     callback();
                 });
