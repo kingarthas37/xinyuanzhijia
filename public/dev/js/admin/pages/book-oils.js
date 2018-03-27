9f4b770ae1ba184879701bd0d3b4d03ce2f077ce
Ralf Wang <ralf@extrabux.com>
2018-03-27 16:33:49 +0800

    git

---
+++ public/dev/js/admin/pages/book-oils.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let leanAppHeader = window.leanAppHeader;
+let utils = require('../common/utils');
+
+module.exports = {
+    indexFun() {
+        $('h4').each(function (i,n) {
+            let text = $(n).text();
+            text = text.replace('**小提示:** 由于此类产品出货量较大，时有出现缺货的情况，如暂无现货，建议亲可以选择同类具有相同作用的产品替代，也会有同样的反馈哦，详情可询问暮雪的心愿城客服为您推荐:)','');
+            text = text.replace('**[暮雪的心愿城英译]:**','');
+            $(n).text(text);
+        });
+    }
+};
