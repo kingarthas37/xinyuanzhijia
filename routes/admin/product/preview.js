45b8cf612c06e05d55484aad6362c55200bbcbae
charlesWu <465747959@qq.com>
2018-06-25 23:17:32 +0800

    fix bug

--- routes/admin/product/preview.js
+++ routes/admin/product/preview.js
@@ -53,9 +53,8 @@ router.get('/:productId',(req,res)=> {
                 
                 //水印判断不同淘宝店铺
                 let watermark = productMethodId === 21 ? config.watermark.muxue928 : config.watermark.main;
-                let detailImage = product.get('detailImage').replace(/ac-QuiPuWpJ.clouddn.com/gi,'lc-QuiPuWpJ.cn-n1.lcfile.com').replace(/\.jpg/gi,'.jpg'+ watermark);
+                let detailImage = product.get('detailImage').replace(/ac-QuiPuWpJ.clouddn.com/gi,'lc-QuiPuWpJ.cn-n1.lcfile.com').replace('.jpg','.jpg'+ watermark).replace('.JPG','.JPG'+ watermark);
                 //detailImage += watermark;
-                console.log(detailImage);
                 data = extend(data,{
                     product,
                     name:markdown.toHTML(name),
