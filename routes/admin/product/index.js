b0104d78a89907d7dc09fe0003ab58aa38e79694
charlesWu <465747959@qq.com>
2018-07-07 14:20:53 +0800

    add get-stock

--- routes/admin/product/index.js
+++ routes/admin/product/index.js
@@ -725,4 +725,21 @@ router.post('/set-is-sales/:productId',(req,res)=> {
     });
 });
 
+router.get('/get-stock', (req,res) => {
+    let productId = parseInt(req.query['product-id']) || 0;
+    if (productId>0) {
+        let query = new AV.Query(Product);
+        query.equalTo('productId',productId);
+        query.first().then(result => {
+            if(result.get('stock') > 0) {
+                res.send({success:1,data:true});
+            } else {
+                res.send({success:1,data:false});
+            }
+        });
+    } else {
+        res.send({success:0, data:'ID错误'});
+    }
+});
+
 module.exports = router;
