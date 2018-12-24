f24b5e2cd1bfb80abadee5af770baf9868628e45
charles wu <465747959@qq.com>
2018-12-24 11:50:18 +0800

    up

--- routes/default/article/index.js
+++ routes/default/article/index.js
@@ -89,7 +89,6 @@ router.get('/', (req, res) => {
             });
         })
     ).then(() => {
-        console.log(data);
         let ua =new RegExp('Mobile');
         let views = 'default/article';
         if(ua.test(req.headers['user-agent'])){
